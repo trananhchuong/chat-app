dcac7095f11879c892bb3f3be119f8606b572443
chapter <chapter@192.168.1.14>
2024-09-09 22:37:41 +0700

    feat: add api for auth

---
+++ back-end/controllers/message.controller.js
@@ -0,0 +1,21 @@
+import Conversation from "../models/conversation.model.js";
+import Message from "../models/message.model.js";
+import { getReceiverSocketId, io } from "../socket/socket.js";
+
+export const sendMessage = async (req, res) => {
+  try {
+   
+  } catch (error) {
+    console.log("Error in sendMessage controller: ", error.message);
+    res.status(500).json({ error: "Internal server error" });
+  }
+};
+
+export const getMessages = async (req, res) => {
+  try {
+   
+  } catch (error) {
+    console.log("Error in getMessages controller: ", error.message);
+    res.status(500).json({ error: "Internal server error" });
+  }
+};
--- back-end/server.js
+++ back-end/server.js
@@ -1,6 +1,8 @@
 import express from "express";
 import dotenv from "dotenv";
 import authRoutes from "./routes/auth.routes.js";
+import connectToMongoDB from "./db/connectToMongoDB.js";
+import cookieParser from "cookie-parser";
 
 const app = express();
 
@@ -9,10 +11,16 @@ dotenv.config();
 // PORT should be assigned after calling dotenv.config() because we need to access the env variables.
 const PORT = process.env.PORT || 8000;
 
+app.use(express.json()); // to parse the incoming requests with JSON payloads (from req.body)
+app.use(cookieParser());
+
 app.use("/api/auth", authRoutes);
 
 app.get("/", (req, res) => {
   console.log("🚀 ~ app.get ~ Hello World!");
 });
 
-app.listen(PORT, () => console.log(`Server Running on port ${PORT}`));
+app.listen(PORT, () => {
+  connectToMongoDB();
+  console.log(`Server Running on port ${PORT}`);
+});
