356ba919e20ecf09d659810268837ae65f52a81d
chapter <chapter@192.168.1.14>
2024-09-11 06:16:25 +0700

    feat: add api of users route

--- back-end/server.js
+++ back-end/server.js
@@ -1,7 +1,10 @@
 import express from "express";
 import dotenv from "dotenv";
+
 import authRoutes from "./routes/auth.routes.js";
 import messageRoutes from "./routes/message.routes.js";
+import userRoutes from "./routes/user.routes.js";
+
 import connectToMongoDB from "./db/connectToMongoDB.js";
 import cookieParser from "cookie-parser";
 
@@ -17,6 +20,7 @@ app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
+app.use("/api/users", userRoutes);
 
 app.get("/", (req, res) => {
   console.log("🚀 ~ app.get ~ Hello World!");
